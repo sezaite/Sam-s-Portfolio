9b88f5bdd974d11f0f820cd5a0979a60eccb92fb
sezaite <martyna.sezaite@gmail.com>
2021-01-31 20:48:59 +0000

    ??

--- js/data/albumData.js
+++ js/data/albumData.js
@@ -1,6 +1,6 @@
 const albumData = [
     {
-        imgLink: '/mai.jpg',
+        imgLink: '/main.jpg',
         titles: ['mixing', 'producing', 'singing'],
         description: ' Ex architecto iusto, cumque quidem aut magni exercitationem laudantium vero obcaecati soluta, molestiae excepturi autem atque, natus dicta tempora? Libero distinctio delectus repudiandae nobis itaque inventore architecto aut! Quia, odio?',
         spotifyLink: 'src="https://open.spotify.com/embed/track/50yIjigJPcVQQxrHHeai4H" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"',
