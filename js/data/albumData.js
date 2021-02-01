a82f7b04eedc62bfc562d3f4bd752b65678d05ac
sezaite <martyna.sezaite@gmail.com>
2021-02-01 08:03:01 +0000

    paths

--- js/data/albumData.js
+++ js/data/albumData.js
@@ -1,13 +1,13 @@
 const albumData = [
     {
-        imgLink: '/img1.jpg',
+        imgLink: 'img1.jpg',
         titles: ['mixing', 'producing', 'singing', 'something else'],
         description: ' Ex architecto iusto, cumque quidem aut magni exercitationem laudantium vero obcaecati soluta, molestiae excepturi autem atque, natus dicta tempora? Libero distinctio delectus repudiandae nobis itaque inventore architecto aut! Quia, odio?',
         spotifyLink: 'src="https://open.spotify.com/embed/track/50yIjigJPcVQQxrHHeai4H" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"',
         isInHeroSection: true,
     },
     {
-        imgLink: '/img2.jpg',
+        imgLink: 'img2.jpg',
         titles: ['mixing', 'producing', 'singing', 'picking monsters'],
         description: ' Ex architecto iusto, cumque quidem aut magni exercitationem laudantium vero obcaecati soluta, molestiae excepturi autem atque, natus dicta tempora? Libero distinctio delectus repudiandae nobis itaque inventore architecto aut! Quia, odio?',
         spotifyLink: 'src="https://open.spotify.com/embed/track/50yIjigJPcVQQxrHHeai4H" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"',
@@ -15,7 +15,7 @@ const albumData = [
     },
 
     {
-        imgLink: '/img1.jpg',
+        imgLink: 'img1.jpg',
         titles: ['mixing', 'producing', 'singing'],
         description: ' Ex architecto iusto, cumque quidem aut magni exercitationem laudantium vero obcaecati soluta, molestiae excepturi autem atque, natus dicta tempora? Libero distinctio delectus repudiandae nobis itaque inventore architecto aut! Quia, odio?',
         spotifyLink: 'src="https://open.spotify.com/embed/track/50yIjigJPcVQQxrHHeai4H" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"',
@@ -23,7 +23,7 @@ const albumData = [
     },
 
     {
-        imgLink: '/img4.jpg',
+        imgLink: 'img4.jpg',
         titles: ['mixing', 'producing', 'singing'],
         description: ' Ex architecto iusto, cumque quidem aut magni exercitationem laudantium vero obcaecati soluta, molestiae excepturi autem atque, natus dicta tempora? Libero distinctio delectus repudiandae nobis itaque inventore architecto aut! Quia, odio?',
         spotifyLink: 'src="https://open.spotify.com/embed/track/50yIjigJPcVQQxrHHeai4H" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"',
@@ -32,7 +32,7 @@ const albumData = [
     },
 
     {
-        imgLink: '/img3.jpg',
+        imgLink: 'img3.jpg',
         titles: ['mixing', 'producing', 'singing'],
         description: ' Ex architecto iusto, cumque quidem aut magni exercitationem laudantium vero obcaecati soluta, molestiae excepturi autem atque, natus dicta tempora? Libero distinctio delectus repudiandae nobis itaque inventore architecto aut! Quia, odio?',
         spotifyLink: 'src="https://open.spotify.com/embed/track/50yIjigJPcVQQxrHHeai4H" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"',
@@ -40,14 +40,14 @@ const albumData = [
     },
 
     {
-        imgLink: '/img1.jpg',
+        imgLink: 'img1.jpg',
         titles: ['mixing', 'producing', 'singing'],
         description: ' Ex architecto iusto, cumque quidem aut magni exercitationem laudantium vero obcaecati soluta, molestiae excepturi autem atque, natus dicta tempora? Libero distinctio delectus repudiandae nobis itaque inventore architecto aut! Quia, odio?',
         spotifyLink: 'src="https://open.spotify.com/embed/track/50yIjigJPcVQQxrHHeai4H" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"',
         isInHeroSection: false,
     },
     {
-        imgLink: '/img1.jpg',
+        imgLink: 'img1.jpg',
         titles: ['mixing', 'producing', 'singing'],
         description: 'Libero distinctio delectus repudiandae nobis itaque inventore architecto aut! Quia, odio?',
         spotifyLink: 'src="https://open.spotify.com/embed/track/50yIjigJPcVQQxrHHeai4H" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"',
@@ -55,14 +55,14 @@ const albumData = [
     },
 
     {
-        imgLink: '/img3.jpg',
+        imgLink: 'img3.jpg',
         titles: ['mixing', 'producing', 'singing'],
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro, explicabo placeat! Reprehenderit voluptate ratione praesentium quibusdam dignissimos??',
         spotifyLink: 'src="https://open.spotify.com/embed/track/50yIjigJPcVQQxrHHeai4H" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"',
         isInHeroSection: true,
     },
     {
-        imgLink: '/img2.jpg',
+        imgLink: 'img2.jpg',
         titles: ['mixing', 'producing', 'singing'],
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro, explicabo placeat! Reprehenderit voluptate ratione praesentium quibusdam dignissimos?  Libero distinctio delectus repudiandae nobis itaque inventore architecto aut! Quia, odio?',
         spotifyLink: 'src="https://open.spotify.com/embed/track/50yIjigJPcVQQxrHHeai4H" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"',
