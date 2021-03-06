3483d1eaee0bd81367a1cd1ca4d410d3a928630b
sezaite <martyna.sezaite@gmail.com>
2021-03-06 14:21:48 +0000

    comments deleted

--- js/components/burgerAnimation.js
+++ js/components/burgerAnimation.js
@@ -1,26 +1,22 @@
 function burgerAnimation() {
     const burger = document.querySelector(".burger");
-    const navBlocks = document.querySelectorAll("nav");
+    const nav = document.querySelector("nav");
     const navLinks = document.querySelectorAll("nav a");
-    console.log(navLinks);
-    console.log(navBlocks);
 
     burger.addEventListener("click", () => {
-        console.log('click');
-        navBlocks.forEach((block) => {
-            block.classList.toggle("nav-active");
+        nav.classList.toggle("nav-active");
 
-            navLinks.forEach((link, index) => {
-                if (link.style.animation) {
-                    link.style.animation = ""
-                } else {
-                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
-                }
-            });
-            burger.classList.toggle("x");
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+            }
         });
+        burger.classList.toggle("x");
+    });
 
-    }
-    );
 }
+
+
 export { burgerAnimation }
